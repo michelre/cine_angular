04fa7b8e9b2fe36923f535c55b6ef294316cd43d
remimichel <remi.michel38@gmail.com>
2014-10-22 00:16:28 +0200

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo '*** SERVER INSTALLATION ***'
-sudo npm install express --save
+sudo npm install express
 sudo npm install moviedb 
 
 echo '*** CLIENT INSTALLATION ***'
 sudo yo angular
-sudo npm install grunt-autoprefixer --save-dev
-sudo npm install karma --save-dev
+sudo npm install grunt-autoprefixer
+sudo npm install karma
