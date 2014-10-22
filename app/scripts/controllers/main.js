777ea24bf37911b8ca8406faf038fe30f5c9b6b2
remimichel <remi.michel38@gmail.com>
2014-10-22 09:44:26 +0200

    update controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,21 @@
  * Controller of the coursExoApp
  */
 angular.module('coursExoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, serviceAjax) {
+        $scope.currentPage = 1;
+        $scope.totalPages = 0;
+
+        var loadMovies = function(){
+            $scope.loading = true;
+            serviceAjax.popular($scope.currentPage).success(function(data){
+                $scope.movies = data.results;
+                $scope.totalPages = data.total_pages;
+                $scope.loading = false;
+            });
+        };
+
+        $scope.pageChanged = function(){
+            loadMovies();
+        };
+        loadMovies();
   });
