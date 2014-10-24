17c59dcbbedae4dc32a6e8934329de7ea3f2e403
remimichel <remi.michel38@gmail.com>
2014-10-24 11:10:55 +0200

    begin tests

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -13,7 +13,7 @@ angular.module('coursExoApp')
         $scope.currentPage = 1;
         $scope.totalPages = 0;
 
-        var loadMovies = function(){
+        $scope.loadMovies = function(){
             serviceAjax.search($scope.query, $scope.currentPage).success(function(data){
                 $scope.movies = data.results;
                 $scope.totalPages = data.total_pages;
@@ -21,9 +21,9 @@ angular.module('coursExoApp')
         };
 
         $scope.pageChanged = function(){
-            loadMovies();
+            $scope.loadMovies();
         };
 
-        loadMovies();
+        $scope.loadMovies();
     });
 
